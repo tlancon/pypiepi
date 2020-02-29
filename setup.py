187591f0a4795c61a69166b8fb6d251054f69d3a
tlancon <tlancon@gmail.com>
2020-02-29 12:05:45 -0600

    Add setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fc:
+    long_description = fc.read()
+with open('requirements.txt', 'r') as fc:
+    requirements = [line.strip() for line in fc]
+
+setuptools.setup(
+    name='pypiepi',
+    version='0.0.1',
+    author='Trevor Lancon',
+    description='A library for everything you need to simulate pi on pictures of pies in Python.',
+    long_description=long_description,
+    long_description_content_type='text/md',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.6',
+    install_requires=requirements
+)
